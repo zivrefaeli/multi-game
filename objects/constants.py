9d6dfa841007a00c8f3d27ac3c47128b033b69bd
refaeli <refaeliziv@gmail.com>
2022-11-18 12:07:15 +0200

    update const

--- objects/constants.py
+++ objects/constants.py
@@ -1,16 +1,5 @@
-from enum import Enum
-
 # General Constants
 ROUND_NUMBERS = 2
-SOURCE_FILE = 'source file'
-
-class Data(Enum):
-    ID = 'id'
-    POS = 'position'
-    COLOR = 'color'
-    ANGLE = 'angle'
-    HEALTH = 'health'
-    BULLETS = 'bullets'
 
 # Window Constants
 WIDTH, HEIGHT = 500, 500 # px
@@ -22,6 +11,11 @@ TRANSPARENT = (0, 0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
+
+def source_file(file: str) -> None:
+    filename = file.split('\\')[-1]
+    print(f'{filename} source file')
+
+
 if __name__ == '__main__':
-    filename = __file__.split('\\')[-1]
-    print(f'{filename} {SOURCE_FILE}')
+    source_file(__file__)
