04bc5c5b561dfb44cad87bb044fb9c1877f74b08
refaeli <refaeliziv@gmail.com>
2022-11-19 23:09:13 +0200

    add rotate method

--- objects/dot.py
+++ objects/dot.py
@@ -1,4 +1,5 @@
-from math import sqrt
+from math import sqrt, cos, sin, atan, degrees, radians, floor
+from typing_extensions import Self
 from .constants import ROUND_NUMBERS, WIDTH, HEIGHT
 
 
@@ -43,6 +44,21 @@ class Dot:
             return -1
         d = sqrt((self.x - dot.x) ** 2 + (self.y - dot.y) ** 2)
         return round(d, ROUND_NUMBERS)
+
+    def rotate(self, origin: Self, angle: int) -> tuple:
+        a = self.x - origin.x
+        b = self.y - origin.y
+        r = sqrt(a ** 2 + b ** 2)
+        
+        try:
+            alpha = degrees(atan(b / a))
+        except ZeroDivisionError:
+            alpha = 90 if b > 0 else 270
+    
+        beta = radians(alpha - angle)
+        x, y = floor(r * cos(beta)), floor(r * sin(beta))
+        
+        return x, y
 
     def __str__(self) -> str:
         return f'({self.x}, {self.y})'
