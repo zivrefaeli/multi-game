3899214d9c09ee72e566aa49264c23bec90fedd2
refaeli <refaeliziv@gmail.com>
2022-11-19 23:01:08 +0200

    modify server data

--- server/data.py
+++ server/data.py
@@ -1,2 +1,3 @@
-CONNECTED_USERS = set() # list of conncted users' ids
-DATABASE = dict()       # map of [id] = user data
+CONNECTED_CLIENTS = set() # set of conncted users' ids
+CLIENTS_DATA = dict()     # map of [id] = user json
+CLIENTS_HEALTH = dict()   # map of [id] = user health
