3f235a7a5b42f32feee9060427f9014391fb29cb
refaeli <refaeliziv@gmail.com>
2022-11-19 23:00:50 +0200

    order constants

--- objects/constants.py
+++ objects/constants.py
@@ -1,5 +1,15 @@
 # General Constants
 ROUND_NUMBERS = 2
+VALID = 'valid'
+INVALID = 'invalid'
+
+class ServerData:
+    CLIENTS = 'clients' # dict
+    HEALTH = 'health'   # int
+
+class ClientData:
+    JSON = 'json'           # dict
+    DAMAGE_TO = 'damage_to' # dict
 
 # Window Constants
 WIDTH, HEIGHT = 500, 500 # px
