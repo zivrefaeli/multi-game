7e48a4da6e91a130b59db813e4d2604bb1240ca7
refaeli <refaeliziv@gmail.com>
2022-11-19 23:08:41 +0200

    update clone

--- objects/clone.py
+++ objects/clone.py
@@ -4,7 +4,9 @@ from .player import Player, Json
 class Clone(Player):
     def __init__(self, json: dict) -> None:
         super().__init__(json[Json.ID])
-
+        self.update_json(json)
+    
+    def update_json(self, json: dict) -> None:
         self.position = json[Json.POS]
         self.color = json[Json.COLOR]
         self.angle = json[Json.ANGLE]
