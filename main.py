acc4858a8eb2de621a93e66f38470f78d0aaebb3
refaeli <refaeliziv@gmail.com>
2022-12-09 16:02:29 +0200

    fix main commands

--- main.py
+++ main.py
@@ -5,29 +5,22 @@ from server import smain
 
 FILE_NAME = __file__.split('\\')[-1]
 VALID_COMMANDS = ['s', 'server', 'c', 'client']
-VALID_STARTS = ['--', '-', '']
+COMMAND_START = '-'
 
 
 def main() -> None:
     length = len(sys.argv)
 
     if length <= 1:
-        print(f'Usage: python ./{FILE_NAME} <command>')
-    if length > 2:
+        print(f'Usage: python {FILE_NAME} -<command>')
+    elif length > 2:
         print('Expected only one command')
     if length != 2:
         print('Valid commands are:', ', '.join(VALID_COMMANDS))
         return
 
     command = sys.argv[1].lower()
-    valid = False
-    
-    for start in VALID_STARTS:
-        if command.startswith(start):
-            valid = command[len(start):] in VALID_COMMANDS
-            break
-    
-    if not valid:
+    if command[0] != COMMAND_START or command[1:] not in VALID_COMMANDS:
         print(f'{sys.argv[1]} is an invalid command')
         return
     
